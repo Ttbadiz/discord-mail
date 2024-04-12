606c41da9598c06ea84032e23aafb53221e88984
Ttbadiz <166860298+Ttbadiz@users.noreply.github.com>
2024-04-12 21:16:54 +0300

    Update config.js
    .

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 // @ts-check
 /** @type {import("./jsdoc.js").ConfigData} */
 export default {
-	token: "",
-	channelId: "",
+	token: "MTIyODA0Nzg2NTQzMTk4MjEzMQ.G-BfRI.NUo0mWGmiQkDHJZV1NFOZ2dNhEWG10_EXvH_ZY",
+	channelId: "https://discord.com/channels/1226566807364698112/1226572894344315123",
 	newRole: "", // Verilecek rolünüz bulunmuyorsa boş bırakınız.
 	oldRole: "", // Alınacak rolünüz yoksa boş bırakınız.
 	mail: {
-		username: "", // Hotmail adresiniz
-		password: "", // Hotmail şifreniz
+		username: "", // bozdaglibaris3@gmail.com
+		password: "", // 4946.brs2121
 	},
 };
